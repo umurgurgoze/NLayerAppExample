57f25c421c79d16329b9252784b9612b5de91462
Umur Gürgöze <86147088+umurgurgoze@users.noreply.github.com>
2022-02-19 23:22:27 +0300

    Update
    Update on Crud Actions and Service Layer

---
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+        [JsonIgnore] // => Sen bunu Json'a dönüştürürken ignore et.Client bunu görmesin.Bu anlamda kullanılır.
+        public int StatusCode { get; set; }
+        public List<String> Errors { get; set; }
+        public static CustomResponseDto<T> Success(int statusCode, T data) // Bir T değeri de döndüğünde
+        {
+            return new CustomResponseDto<T> { Data = data, StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Success(int statusCode) // Bir T data dönmediğinde, sadece status code oluştuğunda
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors) // Birden fazla error gelirse
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = errors };
+        }
+        public static CustomResponseDto<T> Fail(int statusCode, string error) // Tek bir error dönerse
+        {
+            return new CustomResponseDto<T> { StatusCode = statusCode, Errors = new List<string> { error } };
+        }
+
+    }
+}
---
+++ NLayer.Service/Services/Service.cs
@@ -0,0 +1,76 @@
+﻿using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitofwork;
+
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitofwork)
+        {
+            _repository = repository;
+            _unitofwork = unitofwork;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitofwork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _repository.AddRangeAsync(entities);
+            await _unitofwork.CommitAsync();
+            return entities;
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+           return await _repository.AnyAsync(expression);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _repository.GetAll().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task RemoveAysnc(T entity)
+        {
+            _repository.Remove(entity);
+            await _unitofwork.CommitAsync();
+        }
+
+        public async Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            _repository.RemoveRange(entities);
+            await _unitofwork.CommitAsync();
+        }
+        
+        public async Task UpdateAsync(T entity)
+        {
+            _repository.Update(entity);
+            await _unitofwork.CommitAsync();
+        }
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _repository.Where(expression);
+        }
+    }
+}
